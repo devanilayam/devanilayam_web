7966dfb3c1af0ec1002c45fb52d3bacd7e760d87
Mouli Bheemaneti <moulibheemaneti99@gmail.com>
2025-12-07 10:53:38 +0530

    Add Vercel Speed Insights integration
    Integrated @vercel/speed-insights into the default layout to enable performance monitoring. Updated package.json and yarn.lock to include the new dependency.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,15 @@
 <!-- eslint-disable vue/no-multiple-template-root -->
 <template>
+   <speed-insights />
    <MyHeader />
    <SideMenu />
    <NuxtPage />
    <MyFooter />
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
 
 <style lang="scss">
 #__nuxt {
